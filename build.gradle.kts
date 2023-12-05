9ed591ddf1588338960e928646484f8ae9dbce47
vadwwxz <vadwwxz@gmail.com>
2023-12-05 18:23:48 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+    `java-gradle-plugin`
+    id("maven-publish")
+}
+
+group = "com.sliderzxc.gradle"
+version = "0.0.1"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    compileOnly("com.android.tools.build:gradle:8.0.2")
+    compileOnly("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.15.0")
+    compileOnly("org.jetbrains.kotlinx:binary-compatibility-validator:0.13.2")
+    compileOnly("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.3")
+    testImplementation(kotlin("test"))
+}
+
+gradlePlugin {
+    plugins.create(project.name) {
+        id = "com.sliderzxc.gradle.setup"
+        implementationClass = "com.sliderzxc.gradle.GradleSetupPlugin"
+    }
+}
