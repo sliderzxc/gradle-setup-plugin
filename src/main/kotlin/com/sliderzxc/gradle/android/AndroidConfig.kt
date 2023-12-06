fa2ec2bccacc97e16dbcf318ddc4db494e32886c
vadwwxz <vadwwxz@gmail.com>
2023-12-06 18:58:51 +0200

    remove useless files

--- src/main/kotlin/com/sliderzxc/gradle/android/AndroidConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.sliderzxc.gradle.android
-
-data class AndroidConfig(
-    val minSdkVersion: Int,
-    val compileSdkVersion: Int,
-    val targetSdkVersion: Int,
-)
--- src/main/kotlin/com/sliderzxc/gradle/core/SetupProject.kt
+++
@@ -1,7 +0,0 @@
-package com.sliderzxc.gradle.core
-
-import org.gradle.api.Project
-
-fun Project.setupProject() {
-    
-}
