9c309aa43bc6d9c191e662441f241e133f35721d
vadwwxz <vadwwxz@gmail.com>
2023-12-10 22:18:29 +0200

    try to require defaults in LocalizationTask

--- src/main/kotlin/com/sliderzxc/gradle/localization/task/LocalizationTask.kt
+++ src/main/kotlin/com/sliderzxc/gradle/localization/task/LocalizationTask.kt
@@ -1,5 +1,7 @@
 package com.sliderzxc.gradle.localization.task
 
+import com.sliderzxc.gradle.defaults.requireDefaults
+import com.sliderzxc.gradle.localization.config.LocalizationConfig
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import java.io.File
@@ -8,9 +10,12 @@ internal abstract class LocalizationTask : DefaultTask() {
 
     @TaskAction
     fun translate() {
-        //project.requireDefaults<>()
+        val languages = project.requireDefaults<LocalizationConfig>()
         val mProjDir = project.layout.projectDirectory.toString()
         val localizedValuesDir = File(mProjDir, "src/main/res/values/main.txt")
         localizedValuesDir.writeText("Hello Localization Plugin")
+        languages.languages.forEachIndexed { index, localizationLanguage ->
+            localizedValuesDir.writeText("index: $index | text: ${localizationLanguage.name}")
+        }
     }
 }
