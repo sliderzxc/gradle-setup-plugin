cd63a5913b3c028cf2952764a8ab6fe8b6d06882
vadwwxz <vadwwxz@gmail.com>
2023-12-11 10:58:47 +0200

    update setupLocalization

--- src/main/kotlin/com/sliderzxc/gradle/localization/SetupLocalization.kt
+++ src/main/kotlin/com/sliderzxc/gradle/localization/SetupLocalization.kt
@@ -1,10 +1,11 @@
 package com.sliderzxc.gradle.localization
 
+import com.sliderzxc.gradle.defaults.extra.getLocalizationConfig
 import com.sliderzxc.gradle.defaults.extra.setExtraLocalization
 import com.sliderzxc.gradle.defaults.requireLocalizations
 import com.sliderzxc.gradle.localization.config.LocalizationConfig
 import org.gradle.api.Project
 
 fun Project.setupLocalization(
-    localizationConfig: LocalizationConfig = requireLocalizations()
+    localizationConfig: LocalizationConfig? = getLocalizationConfig()
 ) = setExtraLocalization(localizationConfig)
