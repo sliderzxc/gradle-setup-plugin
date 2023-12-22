4724fcb0b9aa27cd2c195dde32c1071aa2cd787f
vadwwxz <vadwwxz@gmail.com>
2023-12-22 14:16:24 +0200

    create kotlinDoc for js section

--- src/main/kotlin/com/sliderzxc/gradle/js/SetupJsApp.kt
+++ src/main/kotlin/com/sliderzxc/gradle/js/SetupJsApp.kt
@@ -4,6 +4,12 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
 
+/**
+ * Sets up a Kotlin/JS application for the Gradle project.
+ *
+ * This function configures the Kotlin/JS project extension for the project, enabling the generation
+ * of JavaScript artifacts for the application.
+ */
 fun Project.setupJsApp() {
     extensions.configure<KotlinJsProjectExtension> {
         js(IR) {
