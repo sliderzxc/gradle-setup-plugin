eaceb833e50d76f23b95ea32ea655f050629a6e3
vadwwxz <vadwwxz@gmail.com>
2023-12-23 19:27:23 +0200

    create kotlinDoc for plugin

--- src/main/kotlin/com/sliderzxc/gradle/GradleSetupPlugin.kt
+++ src/main/kotlin/com/sliderzxc/gradle/GradleSetupPlugin.kt
@@ -4,13 +4,21 @@ import com.sliderzxc.gradle.localization.android.task.LocalizationTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/**
+ * Gradle plugin for setting up project configurations.
+ */
 class GradleSetupPlugin : Plugin<Project> {
+    /**
+     * Applies the plugin to the given project.
+     *
+     * @param target The project to apply the plugin to.
+     */
     override fun apply(target: Project) {
         target.tasks.register(
             "generateLocalizations", LocalizationTask::class.java
         ) {
             group = "gradle setup"
-            description = "task generates strings.xml for all your support languages which you add in build.gradle"
+            description = "Task generates strings.xml for all your support languages which you add in build.gradle"
         }
     }
 }
